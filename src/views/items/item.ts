cbb64889ab0a652f5f14b6e70424afe462929609
Case Larsen <clarsen@gmail.com>
2021-01-30 03:32:09 -0800

    bake in icons directly because can't figure how to reference resource in package

--- src/views/items/item.ts
+++ src/views/items/item.ts
@@ -3,6 +3,7 @@
 
 import * as vscode from 'vscode';
 import Utils from '../../utils';
+import * as path from 'path';
 
 /* ITEM */
 
@@ -17,9 +18,19 @@ class Item extends vscode.TreeItem {
 
     this.obj = obj;
     if (this.label.match(/@thisweek/)) {
-      this.iconPath = new vscode.ThemeIcon("calendar");
+      this.iconPath = Utils.view.getIcon("calendar");
+      // this.iconPath = new vscode.ThemeIcon("calendar");
+      // this.iconPath = {
+      //   dark: path.join(__filename, '..', '..', '..', 'resources', 'icons', "calendar.svg"),
+      //   light: path.join(__filename,'..', '..', '..', 'resources', 'icons', "calendar.svg")
+      // };
     } else if (this.label.match(/@today/)) {
-      this.iconPath = new vscode.ThemeIcon("star-full");
+      this.iconPath = Utils.view.getIcon("star-full");
+      // this.iconPath = new vscode.ThemeIcon("star-full");
+      // this.iconPath = {
+      //   dark: path.join(__filename, '..', '..', '..', 'resources', 'icons', "filter_dark.svg"),
+      //   light: path.join(__filename, '..', '..', '..', 'resources', 'icons', "filter_light.svg")
+      // };
     }
   }
 
