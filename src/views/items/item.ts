a544d4db0f910d32a7817952af7d382ba6e39ba6
Case Larsen <clarsen@gmail.com>
2021-01-30 17:23:01 -0800

    fix icon path using Uri, use material icons - no handling of light/dark theme

--- src/views/items/item.ts
+++ src/views/items/item.ts
@@ -18,19 +18,9 @@ class Item extends vscode.TreeItem {
 
     this.obj = obj;
     if (this.label.match(/@thisweek/)) {
-      this.iconPath = Utils.view.getIcon("calendar");
-      // this.iconPath = new vscode.ThemeIcon("calendar");
-      // this.iconPath = {
-      //   dark: path.join(__filename, '..', '..', '..', 'resources', 'icons', "calendar.svg"),
-      //   light: path.join(__filename,'..', '..', '..', 'resources', 'icons', "calendar.svg")
-      // };
+      this.iconPath = Utils.view.getIcon("thisweek");
     } else if (this.label.match(/@today/)) {
-      this.iconPath = Utils.view.getIcon("star-full");
-      // this.iconPath = new vscode.ThemeIcon("star-full");
-      // this.iconPath = {
-      //   dark: path.join(__filename, '..', '..', '..', 'resources', 'icons', "filter_dark.svg"),
-      //   light: path.join(__filename, '..', '..', '..', 'resources', 'icons', "filter_light.svg")
-      // };
+      this.iconPath = Utils.view.getIcon("today");
     }
   }
 
